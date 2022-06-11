8db0cda31018ec7a83486c18da9dc8cc7b01d134
AnaPaurr <39202433@uaz.edu.mx>
2022-06-10 22:31:24 -0500

    Initial commit

---
+++ app/src/main/java/com/example/pet_sit/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.pet_sit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+private WebView myWebView;
+private WebSettings myWebSettings;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = findViewById(R.id.web1);
+        myWebSettings = myWebView.getSettings();
+        myWebSettings.setJavaScriptEnabled(true);
+        myWebSettings.setDomStorageEnabled(true);
+        myWebView.loadUrl("https://pet-sit.herokuapp.com/index.html");
+        myWebView.setWebViewClient(new WebViewClient());
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(myWebView.canGoBack()){
+            myWebView.goBack();
+        }else{
+            super.onBackPressed();
+        }
+    }
+}
